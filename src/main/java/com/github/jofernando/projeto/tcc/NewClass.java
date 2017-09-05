56bfc5e7fd069c7340ffc68311db8ff6bb03d833
20161D13GR0155 <20161D13GR0155@GAR-216970.labs.garanhuns.ifpe>
2017-09-04 21:33:42 -0300

    Teste commit

---
+++ src/main/java/com/github/jofernando/projeto/tcc/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.jofernando.projeto.tcc;
+
+/**
+ *
+ * @author 20161D13GR0155
+ */
+public class NewClass {
+    
+}
