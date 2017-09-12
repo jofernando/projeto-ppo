b55fa009506e33f5b153c38f8926203389311d87
Fernando <fmdinho3@gmail.com>
2017-09-12 09:52:27 -0300

    Alterando metodos e como são implementados

--- src/main/java/com/github/jofernando/projeto/tcc/entidades/Cliente.java
+++ src/main/java/com/github/jofernando/projeto/tcc/entidades/Cliente.java
@@ -6,18 +6,16 @@
 package com.github.jofernando.projeto.tcc.entidades;
 
 import java.io.Serializable;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
 /**
  *
  * @author 20161D13GR0155
  */
-  @Entity
-public class Cliente implements Serializable, Cloneable {
+@Entity
+public class Cliente implements Serializable {
 
     @Id
     @GeneratedValue
@@ -27,63 +25,62 @@ public class Cliente implements Serializable, Cloneable {
     private String telefone;
     private String endereco;
     private String cpf;
-    
-   // @ManyToOne
-   // private UsuarioN usuario;
 
+    // @ManyToOne
+    // private UsuarioN usuario;
     public Cliente() {
     }
 
     public Cliente(String nome, String email, String telefone, String endereco, String cpf) {
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
-        this.endereco = endereco;
-        this.cpf = cpf;
+	this.nome = nome;
+	this.email = email;
+	this.telefone = telefone;
+	this.endereco = endereco;
+	this.cpf = cpf;
     }
 
     public int getId() {
-        return id;
+	return id;
     }
-    
+
     public String getNome() {
-        return nome;
+	return nome;
     }
 
     public void setNome(String nome) {
-        this.nome = nome;
+	this.nome = nome;
     }
 
     public String getEmail() {
-        return email;
+	return email;
     }
 
     public void setEmail(String email) {
-        this.email = email;
+	this.email = email;
     }
 
     public String getTelefone() {
-        return telefone;
+	return telefone;
     }
 
     public void setTelefone(String telefone) {
-        this.telefone = telefone;
+	this.telefone = telefone;
     }
 
     public String getEndereco() {
-        return endereco;
+	return endereco;
     }
 
     public void setEndereco(String endereco) {
-        this.endereco = endereco;
+	this.endereco = endereco;
     }
 
     public String getCpf() {
-        return cpf;
+	return cpf;
     }
 
     public void setCpf(String cpf) {
-        this.cpf = cpf;
+	this.cpf = cpf;
     }
 
     /*public UsuarioN getUsuario() {
@@ -93,59 +90,34 @@ public class Cliente implements Serializable, Cloneable {
     public void setUsuario(UsuarioN usuario) {
         this.usuario = usuario;
     }*/
-
     @Override
     public int hashCode() {
-        int hash = 5;
-        hash = 37 * hash + this.id;
-        hash = 37 * hash + Objects.hashCode(this.nome);
-        hash = 37 * hash + Objects.hashCode(this.email);
-        hash = 37 * hash + Objects.hashCode(this.telefone);
-        hash = 37 * hash + Objects.hashCode(this.endereco);
-        hash = 37 * hash + Objects.hashCode(this.cpf);
-      //  hash = 37 * hash + Objects.hashCode(this.usuario);
-        return hash;
+	int hash = 7;
+	hash = 17 * hash + this.id;
+	return hash;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Cliente other = (Cliente) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        if (!Objects.equals(this.nome, other.nome)) {
-            return false;
-        }
-        if (!Objects.equals(this.email, other.email)) {
-            return false;
-        }
-        if (!Objects.equals(this.telefone, other.telefone)) {
-            return false;
-        }
-        if (!Objects.equals(this.endereco, other.endereco)) {
-            return false;
-        }
-        if (!Objects.equals(this.cpf, other.cpf)) {
-            return false;
-        }
-       /* if (!Objects.equals(this.usuario, other.usuario)) {
-            return false;
-        }*/
-        return true;
+	if (this == obj) {
+	    return true;
+	}
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	final Cliente other = (Cliente) obj;
+	if (this.id != other.id) {
+	    return false;
+	}
+	return true;
     }
-    
+
     @Override
-    public Cliente clone() throws CloneNotSupportedException {
-        return (Cliente) super.clone();
+    public String toString() {
+	return "Cliente{" + "id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", endereco=" + endereco + ", cpf=" + cpf + '}';
     }
-    
+
 }
