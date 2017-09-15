a3557da44c08d13b1875210f66d7c7a2cdb4eb1f
Fernando <fmdinho3@gmail.com>
2017-09-15 00:43:07 -0300

    Adicionando jpa

---
+++ src/main/java/com/github/jofernando/projeto/tcc/util/JpaUtil.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.jofernando.projeto.tcc.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Fernando
+ */
+public class JpaUtil {
+
+    private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("UnidadeDePèrsistencia");
+
+    private JpaUtil() {
+    }
+
+    public static EntityManager getEntityManager() {
+	return FACTORY.createEntityManager();
+    }
+
+    public static void close() {
+	FACTORY.close();
+    }
+}
