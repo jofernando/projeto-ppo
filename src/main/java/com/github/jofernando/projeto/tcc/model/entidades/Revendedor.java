7bc82313c65e596a15a75e752cd6c1f667cdbb37
Fernando <fmdinho3@gmail.com>
2017-09-19 13:44:19 -0300

    Formatando

--- src/main/java/com/github/jofernando/projeto/tcc/model/entidades/Revendedor.java
+++ src/main/java/com/github/jofernando/projeto/tcc/model/entidades/Revendedor.java
@@ -6,6 +6,7 @@
 package com.github.jofernando.projeto.tcc.model.entidades;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import javax.persistence.Entity;
@@ -32,70 +33,73 @@ public class Revendedor extends Usuario implements Serializable {
     }
 
     public Revendedor(String cpf, String username, String password, String nome) {
-        super(username, password, nome);
-        this.cpf = cpf;
+	super(username, password, nome);
+	this.cpf = cpf;
+	clientes = new HashSet<>();
+	pedidos = new HashSet<>();
+	produtos = new HashSet<>();
     }
 
     public String getCpf() {
-        return cpf;
+	return cpf;
     }
 
     public void setCpf(String cpf) {
-        this.cpf = cpf;
+	this.cpf = cpf;
     }
 
     public Set<Cliente> getClientes() {
-        return clientes;
+	return clientes;
     }
 
     public void setClientes(Set<Cliente> clientes) {
-        this.clientes = clientes;
+	this.clientes = clientes;
     }
 
     public Set<Pedido> getPedidos() {
-        return pedidos;
+	return pedidos;
     }
 
     public void setPedidos(Set<Pedido> pedidos) {
-        this.pedidos = pedidos;
+	this.pedidos = pedidos;
     }
 
     public Set<Produto> getProdutos() {
-        return produtos;
+	return produtos;
     }
 
     public void setProdutos(Set<Produto> produtos) {
-        this.produtos = produtos;
+	this.produtos = produtos;
     }
 
     @Override
     public int hashCode() {
-        int hash = 5;
-        hash = 47 * hash + Objects.hashCode(this.cpf);
-        return hash;
+	int hash = 3;
+	hash = 11 * hash + Objects.hashCode(this.cpf);
+	return hash;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Revendedor other = (Revendedor) obj;
-        if (!Objects.equals(this.cpf, other.cpf)) {
-            return false;
-        }
-        return true;
+	if (this == obj) {
+	    return true;
+	}
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	final Revendedor other = (Revendedor) obj;
+	if (!Objects.equals(this.cpf, other.cpf)) {
+	    return false;
+	}
+	return true;
     }
 
     @Override
     public String toString() {
-        return "Revendedor{" + ", cpf=" + cpf + '}';
+	return "Revendedor{" + "cpf=" + cpf + ", clientes=" + clientes + ", pedidos=" + pedidos + ", produtos=" + produtos + '}';
     }
 
 }
